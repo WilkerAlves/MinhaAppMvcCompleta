6b63ec3f6552c9b3a04d48f0c864b25846088390
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2019-05-31 12:45:52 -0300

    correção de namespace

---
+++ src/DevIO.Business/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using DevIO.Business.Models;
+using System;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+
+    }
+}
