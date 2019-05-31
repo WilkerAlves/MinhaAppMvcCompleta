9737cbe6208540feb35bd6a7b48faf1067de5ef4
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2019-05-31 11:48:30 -0300

    configurando o DbContext

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,36 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options)  : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //colocando um valor default para os mapeamentos caso seja necessario
+            foreach(var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                property.Relational().ColumnType = "varchar(100)";
+            }
+
+            //habilitando o mapeamento
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+
+            // impedindo o cascade
+            foreach(var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
